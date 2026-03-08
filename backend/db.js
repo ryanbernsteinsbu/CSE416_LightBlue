feaf872b2b7425505eb78920bd255a21cb9f507a
Ryan Bernstein <ryan@ryanjbernstein.com>
2026-03-08 13:17:38 -0400

    redeploy

--- backend/db.js
+++ backend/db.js
@@ -14,6 +14,6 @@ pool.query('SELECT NOW()', (err, res) => {
   if (err) {
     console.error('Database connection failed:', err);
   } else {
-    console.log('Database connected at:', res.rows[0].now);
+    console.log('Database connected at::', res.rows[0].now);
   }
 });
