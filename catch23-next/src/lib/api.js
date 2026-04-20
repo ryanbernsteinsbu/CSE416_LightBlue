61b2bee212b87235ea514ccc354713c170386d26
Ryan Bernstein <ryan@ryanjbernstein.com>
2026-04-20 01:08:07 -0400

    dont need dotenv

--- catch23-next/src/lib/api.js
+++ catch23-next/src/lib/api.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
-import dotenv from 'dotenv';
-dotenv.config();
-
 const BASE_URL = process.env.BASE_URL;
 axios.defaults.baseURL = BASE_URL;
 axios.defaults.withCredentials = true;
