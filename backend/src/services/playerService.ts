df34e34130a4c994a57677a8d0dcf46581eaa81a
Ryan Bernstein <ryan.bernstein@stonybrook.edu>
2026-05-15 17:38:37 -0400

    added minor search

--- backend/src/services/playerService.ts
+++ backend/src/services/playerService.ts
@@ -67,12 +67,16 @@ export const getPlayersByStatus = async (status: Status): Promise<Player[] | nul
 }
 
 // POST /api/players/query
-export const queryPlayers = async (nameQuery: string, posQuery: string, teamQuery: string, sortKey: string, sortDir: boolean, isHitters: boolean, page: number, pageSize:number): Promise<{players: Player[], total: number} | null> => {
+export const queryPlayers = async (nameQuery: string, posQuery: string, teamQuery: string, sortKey: string, sortDir: boolean, isHitters: boolean, page: number, pageSize:number, minors:boolean): Promise<{players: Player[], total: number} | null> => {
     try {
         const where: any = {};
         
         where.isHitter = isHitters;
-        
+
+        where.status = minors
+            ? "MINORS"
+            : { [Op.ne]: "MINORS" };       
+
         if (nameQuery?.trim()) {
             where[Op.or] = [
                 { firstName: { [Op.iLike]: `%${nameQuery}%` } },
