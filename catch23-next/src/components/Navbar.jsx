60b91ebb453fe21145371d1ba331170c5bb31994
Evelyn Sun <evelynsun632@gmail.com>
2026-05-15 21:49:58 -0400

    edit profile functionality and resetting password

--- catch23-next/src/components/Navbar.jsx
+++ catch23-next/src/components/Navbar.jsx
@@ -6,9 +6,14 @@ import { useRouter, usePathname } from "next/navigation";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const menuRef = useRef(null);
   const router = useRouter();
   const pathname = usePathname();
+
+  useEffect(() => {
+    setIsLoggedIn(!!localStorage.getItem("token"));
+  }, []);
 
   useEffect(() => {
     const onDocClick = (e) => {
@@ -51,7 +56,6 @@ export default function Navbar() {
               type="button"
               onClick={() => {
                 setOpen(false);
-
                 if (pathname === "/") {
                   window.location.href = "/";
                 } else {
@@ -72,16 +76,18 @@ export default function Navbar() {
         )}
       </div>
 
-      <div className="tooltip-wrap tooltip-left" data-tip="View Profile">
-        <button
-          className="navbar-iconbtn"
-          type="button"
-          onClick={() => router.push("/profile")}
-          aria-label="Profile"
-        >
-          <i className="fa-solid fa-user" />
-        </button>
-      </div>
+      {isLoggedIn && (
+        <div className="tooltip-wrap tooltip-left" data-tip="View Profile">
+          <button
+            className="navbar-iconbtn"
+            type="button"
+            onClick={() => router.push("/profile")}
+            aria-label="Profile"
+          >
+            <i className="fa-solid fa-user" />
+          </button>
+        </div>
+      )}
     </header>
   );
 }
